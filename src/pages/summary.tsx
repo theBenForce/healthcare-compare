64698adf1024a39b18863b392e85dc581859ec15
Ben Force <benforce@gmail.com>
2023-11-11 07:26:24 -0600

    fix: add padding bellow disclaimer

--- src/pages/summary.tsx
+++ src/pages/summary.tsx
@@ -7,6 +7,7 @@ import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import useTheme from '@mui/material/styles/useTheme';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { BuyMeACoffee } from '../components/BuyMeACoffee';
 
 export const SummaryPage: React.FC = () => {
   const { setTitle } = useAppContext();
@@ -54,7 +55,7 @@ export const SummaryPage: React.FC = () => {
     setTitle(t('summary.title'));
   }, [setTitle, t]);
 
-  return <Stack spacing={2}>
+  return <Stack spacing={2} sx={{ paddingBottom: 8 }}>
     {Boolean(report.length) && <>
       <BarChart
         xAxis={[{ scaleType: 'band', data: report?.map(({ name }) => name) ?? [] }]}
@@ -71,6 +72,8 @@ export const SummaryPage: React.FC = () => {
     <Typography variant="body1">{t('summary.about.content')}</Typography>
     <Typography variant="h6">{t('summary.disclaimer.title')}</Typography>
     <Typography variant="h6">{t('summary.disclaimer.content')}</Typography>
+
+    {Boolean(report.length) && <BuyMeACoffee />}
   </Stack>;
 };
 
